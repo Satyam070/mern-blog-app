f470870daddeca47d1eb20f11c983dffb4fc9f4d
Satyam070 <https://github.com/Satyam070>
2024-02-17 21:55:51 +0530

    added build code

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const colors = require("colors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const path = require('path')
 
 //env config
 dotenv.config();
@@ -26,6 +27,13 @@ app.use(morgan("dev"));
 //routes
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/blog", blogRoutes);
+
+//static files
+app.use(express.static(path.join(__dirname, './client/build')))
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
 
 // Port
 const PORT = process.env.PORT || 8080;
